1ce35cb1de161dd84b6dc06860002c8fdd5f6ab7
Andrzej Kolanowski <andrzejkolanowski@gmail.com>
2024-02-04 15:47:20 +0100

    Pierwszy program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String imie="Andrzej";
+        int wiek = 33;
+        System.out.println("Cześć "+imie+", mam "+wiek+"!");
     }
 }
