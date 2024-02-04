c4e013fc9f0e01c91436d607a0567364ee00547c
Andrzej Kolanowski <andrzejkolanowski@gmail.com>
2024-02-04 16:07:35 +0100

    Drugi, pobranie wieku i imienia od użytkownika

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String imie="Andrzej";
-        int wiek = 33;
+        System.out.println("Proszę podać imię:");
+        Scanner scanner = new Scanner(System.in);
+        String imie = scanner.nextLine();
+
+        System.out.println("Ile masz lat:");
+        int wiek = scanner.nextInt();
+        //int wiek = Integer.parseInt(scanner.nextLine());
         System.out.println("Cześć "+imie+", mam "+wiek+"!");
     }
 }
